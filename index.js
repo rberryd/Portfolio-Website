d0a2a1a9835ac1c9a694513aecaf777d87a4969c
rberryd <r.berryd@gmail.com>
2024-04-19 16:20:17 -0400

    Updated the dark mode function a little bit. Added some features to the site to see how it looks. Still WIP.

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ light.addEventListener('click', function() {
         document.documentElement.style.setProperty("--navbgcolor", "rgb(22, 22, 22)");
         document.documentElement.style.setProperty("--navtxtcolor", "rgb(219, 219, 219)");
         document.documentElement.style.setProperty("--bgcolor", "rgb(35, 38, 42)");
+        document.documentElement.style.setProperty("--infobgcolor", "rgba(196, 196, 196, 0.8)");
+        document.documentElement.style.setProperty("--desctxtcolor", "rgb(80, 80, 80)");
+        document.documentElement.style.setProperty("--outertxtcolor", "rgb(234, 234, 234)");
         logo.src = "/images/dark_logo.png";
         light.innerText = "light_mode";
         return;
@@ -13,6 +16,9 @@ light.addEventListener('click', function() {
         document.documentElement.style.setProperty("--navbgcolor", "rgb(232, 232, 232)");
         document.documentElement.style.setProperty("--navtxtcolor", "rgb(139, 139, 139)");
         document.documentElement.style.setProperty("--bgcolor", "rgb(127, 141, 156)");
+        document.documentElement.style.setProperty("--infobgcolor", "rgba(117, 117, 117, 0.8)");
+        document.documentElement.style.setProperty("--desctxtcolor", "rgb(255, 255, 255)");
+        document.documentElement.style.setProperty("--outertxtcolor", "rgb(50, 50, 50)");
         logo.src = "/images/light_logo.png";
         light.innerText = "dark_mode";
         return;
